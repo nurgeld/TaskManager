d13534b73b2bc2953ce81e66818bd7d84d3ae300
nurgeld <nurgeldy@gmail.com>
2022-10-16 17:41:30 +0000

    add factories for manager, admin, developer

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -2,4 +2,8 @@ FactoryBot.define do
   sequence :string, aliases: [:first_name, :last_name, :password] do |n|
     "string#{n}"
   end
+
+  sequence :email do |n|
+    "postbox#{n}@examplemail.com"
+  end
 end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -3,8 +3,20 @@ FactoryBot.define do
     first_name
     last_name
     password
-    email { "MyString" }
+    email
     avatar { "MyString" }
     type { "" }
+
+    factory :developer do
+      type { "Developer" }
+    end
+
+    factory :manager do
+      type { "Manager" }
+    end
+
+    factory :admin do
+      type { "Admin" }
+    end
   end
 end
