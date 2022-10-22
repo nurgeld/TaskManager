be10233fe7f3ce52a67977a4e0268c62f50b0c81
nurgeld <nurgeldy@gmail.com>
2022-10-22 15:44:00 +0000

    fix style with rubocop

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,8 +7,7 @@ class Task < ApplicationRecord
   validates :author, presence: true
   validates :description, length: { maximum: 500 }
 
-  state_machine :initial => :new_task do
-
+  state_machine initial: :new_task do
     event :to_development do
       transition [:new_task, :in_qa, :in_code_review] => :in_development
     end
@@ -18,20 +17,19 @@ class Task < ApplicationRecord
     end
 
     event :to_qa do
-      transition :in_development => :in_qa
+      transition in_development: :in_qa
     end
 
     event :to_code_review do
-      transition :in_qa => :in_code_review
+      transition in_qa: :in_code_review
     end
 
     event :to_ready_for_release do
-      transition :in_code_review => :ready_for_release
+      transition in_code_review: :ready_for_release
     end
 
     event :to_release do
-      transition :ready_for_release => :released
+      transition ready_for_release: :released
     end
-
   end
 end
--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     description
     author factory: :manager
     assignee factory: :developer
-    state { "MyString" }
-    expired_at { "2022-10-15" }
+    state { 'MyString' }
+    expired_at { '2022-10-15' }
   end
 end
