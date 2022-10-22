48c539fffaa85ce67fb807bb283fbe907e6cf258
nurgeld <nurgeldy@gmail.com>
2022-10-22 16:59:40 +0000

    fix factory for task states

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -4,7 +4,31 @@ FactoryBot.define do
     description
     author factory: :manager
     assignee factory: :developer
-    state { 'MyString' }
     expired_at { '2022-10-15' }
+    state { :new_task }
+
+    trait :to_development do
+      state { :in_development }
+    end
+
+    trait :to_archive do
+      state { :archived }
+    end
+
+    trait :to_qa do
+      state { :in_qa }
+    end
+
+    trait :to_code_review do
+      state { :in_code_review }
+    end
+
+    trait :to_ready_for_release do
+      state { :ready_for_release }
+    end
+
+    trait :to_release do
+      state { :released }
+    end
   end
 end
